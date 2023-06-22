c2ce15fc47dada058615ae2253eb22022925e6f9
thalles-victor <thalles.victor2012@gmail.com>
2023-06-22 16:45:27 -0300

    :wrench: :books: docs: implemented swagger with yml file support.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import fs from "fs";
+import YAML from "yaml";
+import swaggerUI from "swagger-ui-express";
+import { join } from "path";
 import router from "./Application/Routes/router";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const ymlDoc = fs.readFileSync(join(__dirname, "swagger", "doc.yml"), "utf8");
+const swaggerDoc = YAML.parse(ymlDoc);
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+const PORT = process.env.PORT ?? 3000;
 
 app.use("/", router);
 
