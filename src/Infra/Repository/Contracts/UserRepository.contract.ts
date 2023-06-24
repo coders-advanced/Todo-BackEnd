c605900592fb219d11b84c2602f2cee74a45c23e
thalles-victor <thalles.victor2012@gmail.com>
2023-06-24 11:13:12 -0300

    :recycle: refactor: updated the UserRepository contract.

--- src/Infra/Repository/Contracts/UserRepository.contract.ts
+++ src/Infra/Repository/Contracts/UserRepository.contract.ts
@@ -2,4 +2,5 @@ import { UserEntity } from "../../../Application/Entities/UserEntity";
 
 export interface UserRepositoryContract {
   create(userEntity: UserEntity): Promise<UserEntity>;
+  findByEmail(email: string): Promise<UserEntity | null>;
 }
